51bc8df334879607e8b54012b3c39325c7e6fd44
Wal Monfortt <jose.gual.monfortte.f@gmail.com>
2022-06-07 11:42:07 -0500

    auth layout first container

--- lib/ui/layouts/auth/auth_layout.dart
+++ lib/ui/layouts/auth/auth_layout.dart
@@ -7,11 +7,44 @@ class AuthLayout extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: Center(
-          child: Text('Auth Layout'),
+      body: ListView(
+        children: [
+          // Desktop
+          _DesktopBody(),
+          // mobile
+          // linksbar
+        ]
+      )
+    );
+  }
+}
+
+class _DesktopBody extends StatelessWidget {
+  const _DesktopBody({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+
+    final size = MediaQuery.of(context).size;
+    return Container(
+      // this is necessary because is a child of a list view
+      width: size.width,
+      height: size.height,
+      color: Colors.red,
+      child: Row(children: [
+        // Twitter background
+        Expanded(
+          child: Container(
+            color:Colors.blue,
+          ),
         ),
-      ),
+        // View container
+        Container(
+          width: 600,
+          height: double.infinity,
+          color: Colors.black,
+        )
+      ],)
     );
   }
 }
