2fd46dd8987a001237c70eb21254d9011d7b29a8
Wal Monfortt <71857487+WallMonfortt@users.noreply.github.com>
2022-08-02 16:48:58 -0500

    separator text and custom item

--- lib/ui/shared/sidebar.dart
+++ lib/ui/shared/sidebar.dart
@@ -1,4 +1,6 @@
 import 'package:admin_dashboard/ui/shared/widgets/logo.dart';
+import 'package:admin_dashboard/ui/shared/widgets/menu_item_custom.dart';
+import 'package:admin_dashboard/ui/shared/widgets/text_separator.dart';
 import 'package:flutter/material.dart';
 
 class Sidebar extends StatelessWidget {
@@ -12,10 +14,80 @@ class Sidebar extends StatelessWidget {
       decoration: buidBoxDecoration(),
       child: ListView(
         physics: ClampingScrollPhysics(),
-        children: const [
+        children: [
           Logo(),
           SizedBox(
             height: 50,
+          ),
+          TextSeparator(
+              // This is a generic widget, in this case shows the text 'main'
+              text: 'main'),
+          MenuItemCustom(
+            text: 'Dashboard',
+            icon: Icons.compass_calibration_outlined,
+            isActive: false,
+            onPressed: () => print('dashboard'),
+          ),
+          MenuItemCustom(
+            text: 'Orders',
+            icon: Icons.shopping_cart_outlined,
+            onPressed: () => {},
+          ),
+          MenuItemCustom(
+            text: 'Analitic',
+            icon: Icons.show_chart_outlined,
+            onPressed: () => {},
+          ),
+          MenuItemCustom(
+            text: 'Categories',
+            icon: Icons.layers_outlined,
+            onPressed: () => {},
+          ),
+          MenuItemCustom(
+            text: 'Products',
+            icon: Icons.dashboard_outlined,
+            onPressed: () => {},
+          ),
+          MenuItemCustom(
+            text: 'Discount',
+            icon: Icons.attach_money_outlined,
+            onPressed: () => {},
+          ),
+          MenuItemCustom(
+            text: 'Customers',
+            icon: Icons.people_alt_outlined,
+            onPressed: () => {},
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          TextSeparator(
+              // This is a generic widget, in this case shows the text 'main'
+              text: 'UI Elements'),
+          MenuItemCustom(
+            text: 'Icons',
+            icon: Icons.list_alt_outlined,
+            onPressed: () => {},
+          ),
+          MenuItemCustom(
+            text: 'Marketing',
+            icon: Icons.mark_email_read_outlined,
+            onPressed: () => {},
+          ),
+          MenuItemCustom(
+            text: 'Campaign',
+            icon: Icons.note_add_outlined,
+            onPressed: () => {},
+          ),
+          MenuItemCustom(
+            text: 'Black',
+            icon: Icons.post_add_outlined,
+            onPressed: () => {},
+          ),
+          MenuItemCustom(
+            text: 'Logout',
+            icon: Icons.exit_to_app_outlined,
+            onPressed: () => {},
           ),
         ],
       ),
---
+++ lib/ui/shared/widgets/menu_item_custom.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class MenuItemCustom extends StatefulWidget {
+  final String text;
+  final IconData icon;
+  final bool isActive;
+  final Function onPressed;
+  const MenuItemCustom({
+    Key? key,
+    required this.text,
+    required this.icon,
+    this.isActive = false,
+    required this.onPressed,
+  }) : super(key: key);
+
+  @override
+  State<MenuItemCustom> createState() => _MenuItemCustomState();
+}
+
+class _MenuItemCustomState extends State<MenuItemCustom> {
+  bool isHovered = false;
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedContainer(
+        duration: Duration(milliseconds: 300),
+        color: isHovered
+            ? Colors.black.withOpacity(0.1)
+            : widget.isActive
+                ? Colors.black.withOpacity(0.1)
+                : Colors.transparent, //This the color for the active menu item
+        child: Material(
+          color: Colors.transparent,
+          child: InkWell(
+            onTap: widget.isActive
+                ? null
+                : () => widget
+                    .onPressed(), // This is the onTap function for the menu item if it is not active
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+              child: MouseRegion(
+                onEnter: (_) => setState(() => isHovered = true),
+                onExit: (_) => setState(() => isHovered = false),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Icon(
+                      widget.icon,
+                      color: Colors.white.withOpacity(0.3),
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      widget.text,
+                      style: GoogleFonts.roboto(
+                        color: Colors.white.withOpacity(0.8),
+                        fontSize: 16,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ));
+  }
+}
